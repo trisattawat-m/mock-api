471482a08177c27c8ee8636111759f79ce8c9bda
Trisattawat Mekchay <trisattawat.mekchay@psd.co.th>
2025-10-24 10:34:41 +0700

    Update config.ts
    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,15 +3,15 @@ import { Injectable } from '@nestjs/common';
 require('dotenv').config();
 
 @Injectable()
-export class MsSQLConfig {
+export class DatabaseConfig {
   host: string;
 
   constructor() {
-    const user = process.env.MSSQL_USER || 'sa';
-    const pass = process.env.MSSQL_PASSWORD || 'password';
-    const host = process.env.MSSQL_HOST || 'localhost';
-    const port = process.env.MSSQL_PORT || '1433';
-    const db = process.env.MSSQL_DATABASE || 'wim_dev';
+    const user = process.env.DATABASE_USER || 'sa';
+    const pass = process.env.DATABASE_PASSWORD || 'password';
+    const host = process.env.DATABASE_HOST || 'localhost';
+    const port = process.env.DATABASE_PORT || '1433';
+    const db = process.env.DATABASE_NAME || 'wim_dev';
 
     this.host = `mssql://${user}:${pass}@${host}:${port}/${db}`;
   }
